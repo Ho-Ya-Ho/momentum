24707eaca7e4ca24bb200abd0dfe669842704411
Ho-Ya-Ho <rlarkdgh1056@naver.com>
2021-12-31 04:25:07 +0900

    momentum

---
+++ js/greetings.js
@@ -0,0 +1,27 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden"); //hidden은 css에서 가져온 것
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = "Hello " + username;
+    greeting.classList.remove("hidden");
+}
+
+const savedUsername = localStorage.getItem("username");
+
+if(savedUsername === null){
+    loginForm.classList.remove("hidden");
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    paintGreetings(savedUsername);
+}
+
